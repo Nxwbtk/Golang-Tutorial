d7d4606be630a4055ccfb442759d6159d55f0a8c
nxwbtk <buntakan0703@gmail.com>
2024-02-21 02:14:19 +0700

    Add ref

--- HTTP-Server/fiber/main.go
+++ HTTP-Server/fiber/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// https://youtu.be/nfSbFk8y4D0?si=nrHMvbt6BXB8rorp&t=773
 func main() {
 	app := fiber.New()
 	app.Get("/hello/", func(c *fiber.Ctx) error {
