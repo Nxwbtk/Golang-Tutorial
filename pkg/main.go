63b7d322db9d17b865fc7f83abf476ca6382a6a4
nxwbtk <buntakan0703@gmail.com>
2024-02-20 20:41:10 +0700

    Add comment

--- pkg/main.go
+++ pkg/main.go
@@ -6,6 +6,8 @@ import (
 )
 
 // path of package can copy from go.mod file and add /package-folder-name
+// to call pkg must use name of package and follow with function name ps. folder name can be anything but must import as same as folder name.
+// 1 level can have only 1 package meaning everyfile in same folder must have same package name
 
 func main() {
 	fmt.Println("Hello")
