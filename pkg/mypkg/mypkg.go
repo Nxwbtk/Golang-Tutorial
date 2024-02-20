6c419190ff7dec6723da4dc072c8ebd4c1839a92
nxwbtk <buntakan0703@gmail.com>
2024-02-20 20:45:29 +0700

    Add call pv fn from public fn

--- pkg/mypkg/mypkg.go
+++ pkg/mypkg/mypkg.go
@@ -7,9 +7,12 @@ import (
 // public function must start with capital letter
 func SayHello() {
 	fmt.Println("Hello, From Package!")
+	// call private function
+	sayPV()
 }
 
 // private function must start with small letter
+// but you can call private function from public function
 func sayPV() {
 	fmt.Println("Hello, From PV!")
 }
