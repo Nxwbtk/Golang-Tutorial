2f7970abbd56416915756a71c4ee97116094e2d4
nxwbtk <buntakan0703@gmail.com>
2024-02-23 02:17:01 +0700

    Seperated file

--- HTTP-Server/fiber/main.go
+++ HTTP-Server/fiber/main.go
@@ -6,15 +6,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type Book struct {
-	ID int `json:"id"`
-	Title string `json:"title"`
-	Author string `json:"author"`
-}
-
-var books []Book
-
 // https://youtu.be/nfSbFk8y4D0?si=nrHMvbt6BXB8rorp&t=773
+// run by nodemon --exec go run main.go route.go --signal SIGTERM
 func main() {
 	app := fiber.New()
 
@@ -35,72 +28,4 @@ func main() {
 	app.Delete("/book/:id", deleteBook)
 
 	app.Listen(":8080")
-}
-
-func getAllBook(c *fiber.Ctx) error {
-	return c.JSON(books)
-}
-
-func getBook(c *fiber.Ctx) error {
-	var book_result Book
-	id, err := strconv.Atoi(c.Params("id"))
-	if (err != nil) {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid ID")
-	}
-	for _, book := range books {
-		if (book.ID == id) {
-			book_result = book
-			return c.JSON(book_result)
-		}
-	}
-	return c.Status(fiber.StatusNotFound).SendString("ID Not found")
-}
-
-func createBook(c *fiber.Ctx) error {
-	new_book := new(Book)
-	err := c.BodyParser(new_book);
-	for _, book := range books {
-		if book.ID == new_book.ID {
-			return c.Status(fiber.StatusConflict).SendString("Already have")
-		}
-	}
-	if  err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Body not correct")
-	}
-	books = append(books, *new_book)
-	return c.Status(fiber.StatusCreated).JSON(books)
-}
-
-func updateBook(c *fiber.Ctx) error {
-	updateId, err := strconv.Atoi(c.Params("id"))
-	if (err != nil) {
-		return c.Status(fiber.StatusNotFound).SendString("Not found")
-	}
-	bookBody := new(Book)
-	bodyErr := c.BodyParser(bookBody)
-	if (bodyErr != nil) {
-		return c.Status(fiber.StatusBadRequest).SendString("Body Incorrect")
-	}
-	for i, book := range books {
-		if (book.ID == updateId) {
-			books[i].Title = bookBody.Title
-			books[i].Author = bookBody.Author
-			return c.JSON(books)
-		}
-	}
-	return c.Status(fiber.StatusNotFound).SendString("Not found")
-}
-
-func deleteBook(c *fiber.Ctx) error {
-	deleteId, err := strconv.Atoi(c.Params("id"))
-	if (err != nil) {
-		return c.Status(fiber.StatusNotFound).SendString("Not found")
-	}
-	for i, book := range books {
-		if (book.ID == deleteId) {
-			books = append(books[:i], books[i + 1:]...)
-			return c.SendStatus(fiber.StatusNoContent)
-		}
-	}
-	return c.Status(fiber.StatusNotFound).SendString("Not found")
 }
